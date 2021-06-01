9b7d2703918026db4ccca14de522edb9dc807f43
micro530 <quentin.stac@live.fr>
2021-06-01 17:06:33 +0200

    Création controle et sa structure

---
+++ AP2_MediaTek86/controleur/Controle.cs
@@ -0,0 +1,61 @@
+﻿using AP2_MediaTek86.model;
+using AP2_MediaTek86.vue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2_MediaTek86.controleur
+{
+    class Controle
+    {
+        FrmPersonnel frmPersonnel;
+        FrmAbsences frmAbsences;
+        FrmConnexion frmConnexion;
+        public Controle()
+        {
+
+        }
+        public List<Personnel> GetLesPersonnels()
+        {
+            return null;
+        }
+        public List<Absences> GetLesAbsences(int idpersonnel)
+        {
+            return null;
+        }
+        public List<Motif> GetLesMotif()
+        {
+            return null;
+        }
+        public List<Service> GetLesServices()
+        {
+            return null;
+        }
+        public void Ajouter(object unObjet)
+        {
+
+        }
+        public void Modifier(object unObjet)
+        {
+
+        }
+        public void Supprimer(object unObjet)
+        {
+
+        }
+        public void Authentification(string identifiant, string mdp)
+        {
+
+        }
+        public void AccesAuFrmAbsences()
+        {
+
+        }
+        public void AccesAuFrmPersonnel()
+        {
+
+        }
+    }
+}
