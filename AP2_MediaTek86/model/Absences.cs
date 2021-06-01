a34d0ceb8a600fa80a9bd1f4118d4d286a0fe894
micro530 <quentin.stac@live.fr>
2021-06-01 13:30:27 +0200

    Ajout du package Dal avec la class AccesDonnees et les class métier Personnel, absences, motif et service

---
+++ AP2_MediaTek86/model/Absences.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2_MediaTek86.model
+{
+    class Absences
+    {
+        /// <summary>
+        /// les propriétés de la table absences
+        /// </summary>
+        private int idPersonnel;
+        private DateTime dateDebut;
+        private DateTime dateFin;
+        private int idMotif;
+        private string motif;
+
+        public int IdPersonnel { get => idPersonnel; set => idPersonnel = value; }
+        public DateTime DateDebut { get => dateDebut; set => dateDebut = value; }
+        public DateTime DateFin { get => dateFin; set => dateFin = value; }
+        public int IdMotif { get => idMotif; set => idMotif = value; }
+        public string Motif { get => motif; set => motif = value; }
+        /// <summary>
+        /// Constructeur de la class Absences valorisant les propriétes privées
+        /// </summary>
+        /// <param name="idPersonnel">l'id du personnel auqel l'absence appartient</param>
+        /// <param name="dateDebut">date de début de l'absence</param>
+        /// <param name="dateFin">date de fin de l'absence</param>
+        /// <param name="idMotif">l'id du motif de l'absence</param>
+        /// <param name="motif">le libellé du motif</param>
+        public Absences(int idPersonnel, DateTime dateDebut, DateTime dateFin, int idMotif, string motif)
+        {
+            this.idPersonnel = idPersonnel;
+            this.dateDebut = dateDebut;
+            this.dateFin = dateFin;
+            this.idMotif = idMotif;
+            this.motif = motif;
+        }
+    }
+}
---
+++ AP2_MediaTek86/model/Motif.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2_MediaTek86.model
+{
+    class Motif
+    {
+        /// <summary>
+        /// Propriétés de la table motif 
+        /// </summary>
+        private int idMotif;
+        private string libelle;
+
+        public int IdMotif { get => idMotif; set => idMotif = value; }
+        public string Libelle { get => libelle; set => libelle = value; }
+        /// <summary>
+        /// constructeur de la la class motif
+        /// </summary>
+        /// <param name="idService">l'id unique du motif</param>
+        /// <param name="libelle">son nom</param>
+        public Motif(int idMotif, string libelle)
+        {
+            this.idMotif = idMotif;
+            this.libelle = libelle;
+        }
+        /// <summary>
+        /// redéfinition de la méthode ToString afin de faciliter son utilisation dans les ComboBox
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return libelle;
+        }
+    }
+}
